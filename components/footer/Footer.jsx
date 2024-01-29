135dd9219a0ecd1ffd0864b06d22b5bde7a51b1b
charjkaro <abhijeet.shrivastava@charjkaro.com>
2024-01-29 16:03:17 +0530

    Add newsletter signup route with validation and rate limiting

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 "use client";
 
+import Image from "next/image";
 import { handleSubmit } from "./handleSubmit";
 
 const Footer = () => {
   const handleSubmit = async (event) => {
-    
     event.preventDefault();
 
     const formData = {
@@ -17,6 +17,7 @@ const Footer = () => {
         body: JSON.stringify(formData),
       });
       if (!response.ok) {
+        alert(`HTTP error! status: ${response.status}`)
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
@@ -29,13 +30,14 @@ const Footer = () => {
 
     alert("Thank you for signing up for newsletter");
   };
- 
+
   return (
     <div className="  bg-[url('https://firebasestorage.googleapis.com/v0/b/charjkaro-7a3bc.appspot.com/o/ground.png?alt=media&token=4ad5b176-5662-4baa-80cd-9e59af8f20cb')] bg-cover bg-center">
       <div className="md:px-30 justify-between rounded-3xl rounded-b-none  bg-cover bg-no-repeat p-10 font-poppins text-white md:flex">
-        <img
+        <Image
           src="https://firebasestorage.googleapis.com/v0/b/charjkaro-7a3bc.appspot.com/o/logowhite-removebg-preview.png?alt=media&token=2f63e764-5a4c-474a-a081-5c234e2bf01d"
           alt=""
+          width={400} height={10}
           className="  md:w-[15rem ] md:h-[15rem] "
         />
         <div className="flex flex-col items-start">
@@ -63,6 +65,7 @@ const Footer = () => {
           <form
             netlify="true"
             onSubmit={handleSubmit}
+            type="email"
             className="md:w-48 w-full"
           >
             <input
@@ -89,7 +92,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image width={30} height={30}
                 src="https://cdn1.iconfinder.com/data/icons/bootstrap-fill-vol-2/16/meta-512.png"
                 className="w-6 transition-transform duration-200 ease-in-out hover:translate-x-2 hover:scale-95"
                 alt=""
@@ -100,7 +103,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image width={30} height={30}
                 src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-256.png"
                 className="w-6 transition-transform duration-200 ease-in-out hover:translate-x-2 hover:scale-95"
                 alt=""
@@ -111,7 +114,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image width={30} height={30}
                 src="https://cdn4.iconfinder.com/data/icons/social-icons-16/512/LinkedIn_alt-512.png"
                 className="w-6 transition-transform duration-200 ease-in-out hover:translate-x-2 hover:scale-95"
                 alt=""
@@ -122,7 +125,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image width={30} height={30}
                 src="https://cdn2.iconfinder.com/data/icons/threads-by-instagram/24/x-logo-twitter-new-brand-256.png"
                 className="w-6 transition-transform duration-200 ease-in-out hover:translate-x-2 hover:scale-95"
                 alt=""
@@ -133,7 +136,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image width={30} height={30}
                 src="https://cdn3.iconfinder.com/data/icons/social-media-black-white-2/512/BW_Youtube_glyph_svg-256.png"
                 className="w-6 transition-transform duration-200 ease-in-out hover:translate-x-2 hover:scale-95"
                 alt=""
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["firebasestorage.googleapis.com", "cdn1.iconfinder.com","cdn2.iconfinder.com","cdn3.iconfinder.com","cdn4.iconfinder.com" ],
+  },
+};
 
 export default nextConfig;
