0b9906082bc0ffb73fdb5a48281a2621b58b6eb3
charjkaro <abhijeet.shrivastava@charjkaro.com>
2024-02-07 12:44:17 +0530

    added rate limiter and verification

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,13 +1,37 @@
 import { NextResponse } from "next/server";
+import { get, set } from "lodash";
 
 import { collection, addDoc, doc } from "firebase/firestore";
 import { db } from "@/config/firebase";
 import { data } from "autoprefixer";
 
+const rateLimit = 3; // Number of allowed requests per minute
+const rateLimiter = {};
+
 export async function GET() {
   return NextResponse.json({ message: "Hello World" });
 }
+const rateLimiterMiddleware = (ip) => {
+  const now = Date.now();
+  const windowStart = now - 60 * 1000; // 1 minute ago
+
+  const requestTimestamps = get(rateLimiter, ip, []).filter(
+    (timestamp) => timestamp > windowStart
+  );
+  requestTimestamps.push(now);
+
+  set(rateLimiter, ip, requestTimestamps);
+
+  return requestTimestamps.length <= rateLimit;
+};
 export async function POST(req, res) {
+  const ip = req.headers["x-forwarded-for"];
+
+  if (!rateLimiterMiddleware(ip)) {
+    console.log("too many requests", " Error 429");
+    // return new NextResponse(429).json({ error: "Too many requests" });
+    return Response.json({ error: "too many requests" }, { status: 429 });
+  }
   try {
     const data = await req.json();
     console.log("Received data:", data);
